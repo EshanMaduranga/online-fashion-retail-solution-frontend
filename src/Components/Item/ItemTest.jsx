9a705c93c02e90e1c945630afedb382a775796f2
Eshan <100767773+EshanMaduranga@users.noreply.github.com>
2024-09-22 06:13:13 +0530

    Create ItemTest.jsx

---
+++ src/Components/Item/ItemTest.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import proImg from '../Asset/StaticAssets/img1.jpg'
+
+const ItemTest = ({id, desc, price, img}) => {
+  return (
+    <div className="hmTstProCntCard">
+        <div className="hmTstProCntCardImgCnt">
+            <img src={img} alt="" />
+        </div>
+        <p className="hmTstProCntCardHdTxt">{desc}</p>
+        <span>{price}.00 LKR</span>
+        <div className="freeShp">Free Shipping</div>
+        <div className="cshOnDel">Cash on Delivery</div>
+    </div>
+  )
+}
+
+export default ItemTest
