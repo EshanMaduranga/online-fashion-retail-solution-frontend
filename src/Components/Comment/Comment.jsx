8b6f41e375f962c1cdc028ac1ef685c2af795c77
Eshan <100767773+EshanMaduranga@users.noreply.github.com>
2024-09-22 06:06:40 +0530

    Create Comment.jsx & Comment.css

---
+++ src/Components/Comment/Comment.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import './Comment.css'
+import axios from 'axios'
+
+const Comment = ({comment, uid}) => {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+
+    useEffect(() => {
+        axios.post('http://127.0.0.1:3001/api/user/getuserbyid', {_id: uid})
+        .then(res => {
+            //console.log(uid)
+            setFirstName(res.data.response.firstName)
+            setLastName(res.data.response.lastName)
+        })
+        .catch(error => console.log(error))
+    }, [])
+
+
+  return (
+    <div className="commentcomment" >
+        <div className="commentHeadCont">
+            <span class="material-symbols-outlined">account_circle</span>
+            <h4><span>{firstName}</span></h4>
+            <h4>{lastName}</h4>
+        </div>
+        <div className="commentTxtcont">
+            <p className="commentParaCont">{comment}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Comment
